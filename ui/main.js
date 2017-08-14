157997f8e1a6a4baee3427de541b8952759f2dd4
Chintu Yadav <chintuyadav.sr336@gmail.com>
2017-08-14 22:39:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ shorten.onclick=function(){
         };
         var url = document.getElementById('url-field').value;
         console.log(url);
-        request.open('get', '/createurl', true);
+        request.open('GET', '/createurl', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({url: url}));  
         shorten.value = 'Registering...';
