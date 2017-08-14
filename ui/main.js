52d4ef2a0f1d1a48841337c7f753655eeb2455ce
Chintu Yadav <chintuyadav.sr336@gmail.com>
2017-08-14 19:17:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,9 @@
 //console.log('Loaded!');
 var shorten=document.getElementById('shorten_btn');
 shorten.onclick=function(){
-  var request=new XMLHttpRequest();
-  request.onreadystatechane=function(){
-    if(request.readystate===XMLHttprequest.DONE){
-        if(request.status===200){
-            
-        }
-    }  
-  };
   var input=document.getElementById('url-field');
   var url=input.value;
   console.log(url);
-  request.open('http://google.com');
-  //request.open('POST', 'http://chintuyadavsr336.imad.hasura-app.io/');
+  request.open('POST', 'http://chintuyadavsr336.imad.hasura-app.io/');
   request.send(null);
 };
