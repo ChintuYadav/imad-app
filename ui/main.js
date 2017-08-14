9369168d35d3923b9a17693691018309d6cd50ef
Chintu Yadav <chintuyadav.sr336@gmail.com>
2017-08-14 21:47:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,12 @@ shorten.onclick=function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-        // Typical action to be performed when the document is ready:
-            alert("Error in response");
+            request.open('get', 'http://chintuyadavsr336.imad.hasura-app.io/shorten', true);
+            request.setReqestHeader('Content-Type', 'application/json');
+            request.send();
         }
     };
     console.log(url);
-    request.open('get', 'http://chintuyadavsr336.imad.hasura-app.io/shorten', true);
-    //request.setReqestHeader('Content-Type', 'application/json');
-    request.send();
+    
+    //
 };
