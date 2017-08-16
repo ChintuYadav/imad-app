351953a3b0fc75a29fb72f838d08f3f7564e70bb
Chintu Yadav <chintuyadav.sr336@gmail.com>
2017-08-16 23:08:53 +0530

    [imad-console] Updates js/urlconvert.js

--- js/urlconvert.js
+++ js/urlconvert.js
@@ -1,97 +1,11 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-var Pool = require('pg').Pool;
-var crypto = require('crypto');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-
-var config = {
-    user: 'chintuyadavsr336',
-    database: 'chintuyadavsr336',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
-};
-var pool = new Pool(config);
-
-var app = express();
-app.use(morgan('combined'));
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
-
-app.use(session({
-    secret: 'someRandomSecretValue',
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
-}));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('/ui/wallpaper.png', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'wallpaper.png'));
-});
-
-app.post('/createurl', function(req, res){ 
-    var url = req.body.url;
-    var shortUrl='';
-    var code='';
-    //res.send(url);
-    console.log(url);
-    var count=0;
-    pool.query('SELECT * FROM "Shorten" WHERE "long_url" = $1',[url],function(err, result){
-        if(err){
-            res.status(500).send("Flag: "+err.toString());
-        }
-        else{
-            if(result.rows.length===0){
-                pool.query('INSERT INTO "Shorten" ( "long_url") VALUES ($1);',[url],function(err,result){
-                    if(err){
-                        res.status(500).send("Flag: "+err.toString());  
-                    }
-                    else{
-                        counter = result.rows[0];
-                        flag = "From query";
-                    }
-                    var Id=result.row[0].id;
-                    //code=convert.convertUrl(Id);
-                    res.send({'shortUrl': val});
-                });
-            }
-            else{
-                var Id = result.rows[0].id;
-                //code=convert.convertUrl(Id);
-                res.send({'shortUrl': val});
-            }
-        }
-        
-    });
-    /**/
-
-});
-
-app.get('/:redirect', function(req, res){
-    res.send("The requested page is not found, 404 error");
-});
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+var alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+var base = alphabet.length;
+function converturl(num) {
+    var converted='';
+    while(num){
+        var rem=num%base;
+        num=Math.flooe(num/base);
+        converted=alphabet[rem].toString()+converted;
+    }
+    return converted;
+}
