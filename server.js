b9c24b99056b47a7c1ca818bf71ec979d83e32e8
Chintu Yadav <chintuyadav.sr336@gmail.com>
2017-08-16 20:50:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,18 +70,8 @@ app.post('/createurl', function(req, res){
                 });
             }
             else{
-                pool.query('SELECT (id) FROM "Shorten" WHERE "long_url" =$1',[url], function(error, result1){
-                    if(error){
-                        res.status(500).send("Flag: "+error.toString());
-                    }
-                    else{
-                        count=result1.row[0];
-                        res.send({'count': count});
-                        console.log("count-->"+count);
-                        //res.send({'shortUrl': count});
-                    }
-                });
-                res.send({'shortUrl': url});
+                var id=result.row[0].id;
+                res.send({'shortUrl': id});
             }
         }
         
