0baaf37326f437ed0600bd22f6f364a0da5e97ab
Chintu Yadav <chintuyadav.sr336@gmail.com>
2017-08-17 01:32:50 +0530

    [imad-console] Updates js/urlconvert.js

--- js/urlconvert.js
+++ js/urlconvert.js
@@ -1,6 +1,6 @@
 var alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
 var base = alphabet.length;
-exports.converturl=function (num) {
+function convertUrl (num) {
     var converted='';
     while(num){
         var rem=num%base;
