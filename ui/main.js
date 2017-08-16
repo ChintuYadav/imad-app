0e745f2cc2a46af2fb106e6941e37d71fcf01737
Chintu Yadav <chintuyadav.sr336@gmail.com>
2017-08-16 19:21:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ $('.btn-shorten').on('click', function(){
         // display the shortened URL to the user that is returned by the server
         var resultHTML = '<a class="result" href="' + short_url + '">'
             + data.shortUrl + '</a>';
-        $("#link").html(resultHTML);
+        $("h2").html(resultHTML);
         //$("#link").css({'display':'block'});
         $("#link").slideDown(900);
     }
