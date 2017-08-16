0a9a1377f35492d3e6a712f7b25ce7ac420e215c
Chintu Yadav <chintuyadav.sr336@gmail.com>
2017-08-16 19:08:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ $('.btn-shorten').on('click', function(){
             + data.shortUrl + '</a>';
         $("#link").html(resultHTML);
         //$("#link").css({'display':'block'});
-        $("#link").hide().show("slow");
+        $("#link").fadeIn();
     }
   });
 
