00447fcfacb44bf75eb4e9a1c8af53aae9919ecd
Chintu Yadav <chintuyadav.sr336@gmail.com>
2017-08-17 14:30:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,8 +77,8 @@ app.post('/createurl', function(req, res){
                             Id=Math.floor(Id/base);
                             converted=alphabet[rem].toString()+converted;
                         }
-                        pool.query('INSERT INTO "Shorten" ( "short_url") VALUES ($1);',[converted] ,function(error, result1){
-                            if(error){
+                        pool.query('INSERT INTO "Shorten" ( "short_url") VALUES ($1) WHERE long_url=($2);',[converted, url] ,function(err, result){
+                            if(err){
                                 res.status(500).send("Flag: "+error.toString());
                             }
                             else{
