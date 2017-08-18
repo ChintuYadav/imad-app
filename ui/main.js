bfb791efff14bce870c7823860fdcd8ff7e244c9
Chintu Yadav <chintuyadav.sr336@gmail.com>
2017-08-18 21:03:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,7 @@ $('.btn-shorten').on('click', function(){
         var resultHTML = '<a class="result" href="' + short_url + '" target="_blank">'
             + data.shortUrl + '</a>'+'<button class="btn btn-primary" id="copy_btn">copy</button>';
         //$("#link").html(resultHTML);
+        $('#dialog_title_span').text(html);
         //$("#link").css({'display':'block'});
         $("#link").slideDown(500);
     }
